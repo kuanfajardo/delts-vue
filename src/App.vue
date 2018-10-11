e976f32535614f46665b5eccbb8482c26c3733e7
Juan Diego Fajardo <jfajardo@mit.edu>
2018-10-10 23:25:19 -0400

    Install vuetify and extract vuetify template into seperate components.

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,22 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <app-drawer></app-drawer>
+    <app-toolbar></app-toolbar>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+
+import AppToolbar from './components/AppToolbar'
+import AppDrawer from './components/AppDrawer'
+
+export default {
+  name: 'App',
+  components: { AppDrawer, AppToolbar },
+  data () {
+    return {
+      msg: true
     }
   }
 }
-</style>
+</script>
---
+++ src/components/AppDrawer.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="app-drawer">
+    <v-navigation-drawer
+      persistent
+      clipped="true"
+      v-model="drawer"
+      enable-resize-watcher
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-tile
+          value="true"
+          v-for="(item, i) in items"
+          :key="i"
+        >
+          <v-list-tile-action>
+            <v-icon v-html="item.icon"></v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app-drawer',
+
+  data () {
+    return {
+      items: [
+        {
+          icon: 'add',
+          title: 'Dashboard'
+        },
+        {
+          icon: 'bubble_chart',
+          title: 'Duties'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .app-drawer {
+
+  }
+</style>
