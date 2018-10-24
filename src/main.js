44ab70df310d3cfd4afe7c2e1b5ac7841f3942bf
Juan Diego Fajardo <jfajardo@mit.edu>
2018-10-24 03:21:01 -0400

    Import Firebase into project

--- src/main.js
+++ src/main.js
@@ -5,13 +5,30 @@ import App from './App.vue'
 import router from './router/'
 import store from './store'
 import VeeValidate from 'vee-validate'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
 
+let app;
+let config = {
+  apiKey: 'AIzaSyCYos8q_4IeiPVsuS-2xQkR8wvkXMYQ164',
+  authDomain: 'delts-app.firebaseapp.com',
+  databaseURL: 'https://delts-app.firebaseio.com',
+  projectId: 'delts-app',
+  storageBucket: 'delts-app.appspot.com',
+  messagingSenderId: '1088281406702'
+}
+
+firebase.initializeApp(config)
+
 Vue.use(VeeValidate, { fieldsBagName: 'formFields' })
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+firebase.auth().onAuthStateChanged(function (user) {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+})
