2a1c26fd58197a03ac3cbdb78c7880aa76396ee0
Juan Diego Fajardo <jfajardo@mit.edu>
2018-10-24 15:58:21 -0400

    Add events to application.

---
+++ src/events/index.js
@@ -0,0 +1,73 @@
+import events from './names.js'
+
+export default [
+  // Authentication
+  {
+    name: events.loginSuccess,
+    callback: function (e) {
+      this.snackbar.show = false
+      this.$router.replace(this.$route.query.redirect || '/')
+    }
+  },
+  {
+    name: events.loginFailure,
+    callback: function (e) {
+      this.snackbar = {
+        show: true,
+        color: 'error',
+        text: 'Login error.',
+        timeout: 3000
+      }
+    }
+  },
+  {
+    name: events.authFailed,
+    callback: function (e) {
+      this.$router.push('/login')
+      this.$message.error('Token has expired')
+    }
+  },
+  {
+    name: events.logout,
+    callback: function (e) {
+      this.snackbar = {
+        show: true,
+        color: 'green',
+        text: 'Logout successfully.',
+        timeout: 3000
+      }
+      this.$router.replace({ path: '/login' })
+    }
+  },
+
+  // API Responses
+  {
+    name: events.badRequest,
+    // @error api response data
+    callback: function (msg) {
+      this.$message.error(msg)
+    }
+  },
+  {
+    name: events.accessDenied,
+    // @error api response data
+    callback: function (msg) {
+      this.$message.error(msg)
+      this.$router.push('/forbidden')
+    }
+  },
+  {
+    name: events.resourceDeleted,
+    // @error api response data
+    callback: function (msg) {
+      this.$message.success(msg)
+    }
+  },
+  {
+    name: events.resourceUpdated,
+    // @error api response data
+    callback: function (msg) {
+      this.$message.success(msg)
+    }
+  }
+]
---
+++ src/events/names.js
@@ -0,0 +1,10 @@
+export default {
+  loginSuccess: 'APP_LOGIN_SUCCESS',
+  loginFailure: 'APP_LOGIN_FAILURE',
+  authFailed: 'APP_AUTH_FAILED',
+  logout: 'APP_LOGOUT',
+  badRequest: 'APP_BAD_REQUEST',
+  accessDenied: 'APP_ACCESS_DENIED',
+  resourceDeleted: 'APP_RESOURCE_DELETED',
+  resourceUpdated: 'APP_RESOURCE_UPDATED'
+}
