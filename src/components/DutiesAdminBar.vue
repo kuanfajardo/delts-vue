8f199056c8fa74b7d47fddbf1635d9a5dceaecad
Juan Diego Fajardo <jfajardo@mit.edu>
2018-12-20 10:12:50 -0500

    Implement menu bar for duty picker. Add tabs to Duties Page.

---
+++ src/components/DutiesAdminBar.vue
@@ -0,0 +1,73 @@
+<template>
+  <v-toolbar v-if="showAdmin"  id="admin-bar" class="mb-3 mt-2" style="opacity: 1">
+    <template v-if="tab === 0">
+      <!-- General Actions-->
+      <v-btn color="primary">Go Live
+        <v-icon right>visibility</v-icon>
+      </v-btn>
+
+      <v-btn dark color="teal">Edit Duty Sheet
+        <v-icon dark right>edit</v-icon>
+      </v-btn>
+
+      <v-divider
+        class="mx-3"
+        vertical
+      ></v-divider>
+
+      <!-- Edit Duty Actions -->
+      <v-overflow-btn
+        :items="dropdownNames"
+        label="Edit Assignee"
+        hide-details
+        class="pa-0"
+
+      ></v-overflow-btn>
+
+      <!-- Checkoff Actions -->
+      <template v-if="duty_selected">
+        <v-btn v-if="checked_off" color="error">Undo Checkoff
+          <v-icon dark right>cancel</v-icon>
+        </v-btn>
+
+         <v-btn v-else dark color="#27af6a">Checkoff
+          <v-icon right>check_circle</v-icon>
+        </v-btn>
+      </template>
+    </template>
+    <!-- TODO: Menus for other tabs -->
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  name: 'duties-admin-bar',
+
+  data () {
+    return {
+      duty_selected: true,
+      showAdmin: true,
+      checked_off: false,
+      dropdownNames: [
+        { text: 'Juan' },
+        { text: 'Malik' },
+        { text: 'Mo' },
+        { text: 'Andre' }
+      ]
+    }
+  },
+
+  props: {
+    tab: Number
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  #admin-bar {
+    margin-left: 12.5%;
+    margin-right: 12.5%;
+    max-width: 75%;
+  }
+</style>
--- src/pages/menus/Duties.vue
+++ src/pages/menus/Duties.vue
@@ -1,20 +1,60 @@
 <template>
   <div class="duties-page">
-    <page-header title="Duties"></page-header>
-    <duties-picker></duties-picker>
+    <!-- Tabs -->
+    <v-tabs grow icons-and-text color="transparent" v-model="tab">
+      <v-tabs-slider color="primary"></v-tabs-slider>
+      <v-tab
+        v-for="i in 3"
+        :key="i"
+      >
+        {{tabNames[i - 1]}}
+        <v-icon>{{tabIcons[i - 1]}}</v-icon>
+      </v-tab>
+    </v-tabs>
+
+    <!-- Header -->
+    <v-layout row class="align-center layout px-4 pt-4 app--page-header">
+      <duties-admin-bar :tab="tab"></duties-admin-bar>
+    </v-layout>
+
+    <!-- Main Component -->
+    <component :is="mainComponent"></component>
   </div>
 </template>
 
 <script>
 import PageHeader from '../../components/PageHeader'
 import DutiesPicker from '../../components/DutiesPicker'
+import DutiesAdminBar from '../../components/DutiesAdminBar'
+import DutiesFab from '../../components/DutiesFAB'
 
 export default {
   name: 'duties-page',
-  components: { DutiesPicker, PageHeader },
+  components: { DutiesFab, DutiesAdminBar, DutiesPicker, PageHeader },
   data () {
     return {
-      msg: 'msg'
+      tab: null,
+      tabNames: [
+        'Duty Sheet',
+        'Duties',
+        'Punts'
+      ],
+      tabIcons: [
+        'assignment',
+        'work',
+        'warning'
+      ]
+    }
+  },
+
+  computed: {
+    mainComponent () {
+      switch (this.tab) {
+        case 0:
+          return DutiesPicker
+        default:
+          return null
+      }
     }
   }
 }
