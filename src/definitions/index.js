e3dbc2b1b3ae643ee0506a75a9225d58cb53cce2
Juan Diego Fajardo <jfajardo@mit.edu>
2018-12-22 16:54:45 -0500

    Standardize all reference dates returned by convenience functions to have a time of 00:00:0000.
    This fixes a bug where not all duties in a week were being returned due to being the same date as the ref date, but an earlier time in the day than the ref date.

--- src/definitions/index.js
+++ src/definitions/index.js
@@ -7,14 +7,28 @@ export const DutyStatus = {
 }
 
 export function getLastDayOfWeek (dayOfWeek, refDate = new Date()) {
+  // Get next day-of-week and subtract a week
   var resultDate = new Date(refDate.getTime())
-  resultDate.setDate(refDate.getDate() - 7 + (7 + dayOfWeek - refDate.getDay()) % 7) // +7 to compensate for incorrect JS modulus w negative numbers
+  resultDate.setDate(getNextDayOfWeek(dayOfWeek, refDate).getDate() - 7)
+
+  // Standardize all reference dates to have a time of 00:00:00:0000
+  resultDate.setHours(0, 0, 0, 0)
+
   return resultDate
 }
 
 export function getNextDayOfWeek (dayOfWeek, refDate = new Date()) {
+  // Compute distance to next day-of-week
+  const diff = dayOfWeek - refDate.getDay()
+  const fixedDiff = (diff + 7) % 7 // +7 to compensate for incorrect JS modulus w negative numbers
+
+  // Copy reference date and add the difference needed to get next day-of-week
   var resultDate = new Date(refDate.getTime())
-  resultDate.setDate(refDate.getDate() + (7 + dayOfWeek - refDate.getDay()) % 7) // +7 to compensate for incorrect JS modulus w negative numbers
+  resultDate.setDate(refDate.getDate() + fixedDiff)
+
+  // Standardize all reference dates to have a time of 00:00:00:0000
+  resultDate.setHours(0, 0, 0, 0)
+
   return resultDate
 }
 
