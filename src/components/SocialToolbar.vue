e4ebd84da2bff3202565bb51ca5b866965c46e6a
Juan Diego Fajardo <jfajardo@mit.edu>
2019-01-01 14:09:28 -0500

    Skeleton for Social tab.

---
+++ src/components/SocialToolbar.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-toolbar
+      id="punts-toolbar"
+      class="mb-3 mt-2"
+      style="opacity: 1; width: 900px;"
+  >
+    <template v-if="tab === 0">
+    </template>
+    <template v-if="tab === 1">
+    </template>
+    <template v-if="tab === 2">
+    </template>
+  </v-toolbar>
+</template>
+
+<script>
+import { mapState, mapMutations } from 'vuex'
+
+export default {
+  name: 'social-toolbar',
+
+  //-------------------+
+  //     PROPERTIES    |
+  //-------------------+
+
+  data () {
+    return {
+      msg: 'msg'
+    }
+  },
+
+  computed: {
+    // Local-Computed
+    compute () {
+      return null
+    },
+
+    // Store-Computed
+    ...mapState({})
+  },
+
+  //------------------+
+  //      METHODS     |
+  //------------------+
+
+  methods: {
+    method () {
+      // Do Something
+    },
+
+    ...mapMutations({})
+  },
+
+  // https://vuejs.org/v2/api/#watch
+  watch: {
+    msg: 'someMethod'
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.social-toolbar {
+
+}
+</style>
--- src/pages/menus/Social.vue
+++ src/pages/menus/Social.vue
@@ -1,18 +1,50 @@
 <template>
   <div class="social-page">
-    <page-header title="Social"></page-header>
+    <!-- Tabs -->
+    <v-tabs grow icons-and-text color="transparent" v-model="tab">
+      <v-tabs-slider color="primary"></v-tabs-slider>
+      <v-tab
+        v-for="i in tabNames.length"
+        :key="i"
+      >
+        {{tabNames[i - 1]}}
+        <v-icon>{{tabIcons[i - 1]}}</v-icon>
+      </v-tab>
+    </v-tabs>
+
+    <!-- Header -->
+    <v-layout row class="align-center layout px-4 pt-4 app--page-header">
+      <v-spacer></v-spacer>
+      <social-toolbar :tab="tab"></social-toolbar>
+      <v-spacer></v-spacer>
+    </v-layout>
+
+    <!-- Main Component -->
+    <keep-alive>
+      <component :is="mainComponent"></component>
+    </keep-alive>
   </div>
 </template>
 
 <script>
-import PageHeader from '../../components/PageHeader'
+import SocialToolbar from '../../components/SocialToolbar'
 
 export default {
   name: 'social-page',
-  components: { PageHeader },
+  components: { SocialToolbar },
   data () {
     return {
-      msg: 'msg'
+      tab: null,
+      tabNames: [
+        'Parties',
+        'Invite List',
+        'Bouncing'
+      ],
+      tabIcons: [
+        'local_bar',
+        'face',
+        'assignment'
+      ]
     }
   }
 }
